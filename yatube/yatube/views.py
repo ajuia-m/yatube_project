5918641161f02fe7040a09866f675d3c01d3f909
ajuia-m <user23and@gmail.com>
2023-01-18 23:46:02 +0300

    var name bugs fixed

--- yatube/yatube/views.py
+++ yatube/yatube/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('She was fokin crazy')
+
+def group_posts(request, pk):
+    return HttpResponse(f'She was insane {pk}')
