e0d13c2c15da77c63fac62e1516d837c836f5059
ajuia-m <user23and@gmail.com>
2023-01-22 21:00:05 +0300

    missing css, no posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
-    context = {'text':"Это главная страница проекта Yatube"}
+    context = {
+        'text':"Это главная страница проекта Yatube",
+        'posts': posts,
+        }
     return render(request, template, context) 
 
 def group_list(request):
