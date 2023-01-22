bed80ca884028222174b7ddbb5edbb4c7fbb0ab9
ajuia-m <user23and@gmail.com>
2023-01-22 20:08:12 +0300

    inithmigrations OK

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model): 
     # Тип: TextField
     text = models.TextField() 
-    
-    # Тип поля: DateTimeField, для хранения даты и времени;
-    # параметр auto_now_add определяет, что в поле будет автоматически
-    # подставлено время и дата создания новой записи
     pub_date = models.DateTimeField(auto_now_add=True)
-    # Тип: ForeignKey, ссылка на модель User
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
